ec30771c176b53feb348d0cfe265444435d2111b
ThaoHn <thaohn.pla@gmail.com>
2024-09-17 10:21:15 +0700

    authenticate

--- src/main/java/com/devteria/identity_service/exception/ErrorCode.java
+++ src/main/java/com/devteria/identity_service/exception/ErrorCode.java
@@ -7,6 +7,7 @@ public enum ErrorCode {
     USERNAME_INVALID(1003, "User must be 3 characters"),
     PASSWORD_INVALID(1004,"Password invalid"),
     INVALID_KEY(1005,"Invalid_key"),
+    USER_NOT_EXISTED(1006,"User not existed")
     ;
 
     private int code;
